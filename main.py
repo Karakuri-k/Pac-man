d32094bc5da16187fb01291212fead52e1e8eef5
Karakuri-k <k.kurisakisagberg@gmail.com>
2026-05-19 13:44:07 +0200

    movement pacman hakkete
    Har gjort så pacman kan bevege seg fra rect til rect, skal gjøre movementen smooth

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ clock = pg.time.Clock()
 pacman = PacMan(3, 4)
 
 running = True
+frames = 0
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             running = False
+        elif event.type == pg.KEYDOWN and event.key == pg.K_w:
+            pacman.nextDirection = UP
+        elif event.type == pg.KEYDOWN and event.key == pg.K_a:
+            pacman.nextDirection = LEFT
+        elif event.type == pg.KEYDOWN and event.key == pg.K_s:
+            pacman.nextDirection = DOWN
+        elif event.type == pg.KEYDOWN and event.key == pg.K_d:
+            pacman.nextDirection = RIGHT
+        
+        
 
     # Tegn bakgrunn: (En slags "reset" av hele vinduet vårt)
     vindu.fill(BLACK)
@@ -26,7 +37,8 @@ while running:
     board.draw(vindu)
 
     # TODO: Oppdater objektene våre:
-
+    if frames % 10 == 0:
+        pacman.move(board)
 
     # Tegn objektene våre:
     pacman.draw(vindu)
@@ -35,7 +47,7 @@ while running:
     # Har alltid disse med til slutt:
     pg.display.flip()
     clock.tick(FPS)
-
+    frames += 1
 
 # While running er slutt: Avslutt pygame på en "ryddig måte":
 pg.quit()
